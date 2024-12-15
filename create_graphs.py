9d3f07ccb66cb76828b6b93a5aec8801660e3d11
Christian Hugo Rasmussen <70936273+hugzito@users.noreply.github.com>
2024-12-15 16:03:53 +0100

    Update create_graphs.py
    path changes

--- create_graphs.py
+++ create_graphs.py
@@ -395,179 +395,180 @@ class CityLineGraph:
         # Save the features as a Parquet file
         feats.drop(columns=['geometry', 'geom_type']).to_parquet(f'data/{self.city}/linegraph.parquet', index=False)
 
-def assign_other_attributes(self, G, points, point_ids, feat_name: str, box=None, area=False):
-    # Convert the graph into a DataFrame and drop missing 'to' and 'from' columns
-    gdf = graph_to_dataframe(G).dropna(subset=['to', 'from'])
-    
-    # Iterate through points and their IDs
-    for i, j in zip(points, point_ids):
-        # Print point and ID if ID is None
-        if isinstance(j, type(None)):
-            print(i, j)
-        # Skip if the point is outside the bounding box
-        if not shapely.contains(box, i):
-            continue
-        if area:
-            # If attribute is spatial (e.g., population density), assign it to edges within hexagons
-            for k in zip(gdf.geometry, gdf['from'], gdf['to']):
-                if shapely.contains(i, k[0]):  # Check if hexagon contains edge
-                    if feat_name not in G[k[1]][k[2]]:
-                        G[k[1]][k[2]][feat_name] = [j]
-                    else:
-                        G[k[1]][k[2]][feat_name].append(j)
-                elif shapely.crosses(i, k[0]):  # Check if hexagon crosses edge
-                    if feat_name not in G[k[1]][k[2]]:
-                        G[k[1]][k[2]][feat_name] = [j]
-                    else:
-                        G[k[1]][k[2]][feat_name].append(j)
-        else:
-            # Assign attribute to the nearest edge
-            edge, distance, node_pair = self.find_nearest_edge(gdf.geometry, i, gdf['from'], gdf['to'], box)
-            try:
-                if feat_name not in G[node_pair[0]][node_pair[1]]:
-                    G[node_pair[0]][node_pair[1]][feat_name] = 1
-                else:
-                    G[node_pair[0]][node_pair[1]][feat_name] += 1
-            except:
+    def assign_other_attributes(self, G, points, point_ids, feat_name: str, box=None, area=False):
+        # Convert the graph into a DataFrame and drop missing 'to' and 'from' columns
+        gdf = graph_to_dataframe(G).dropna(subset=['to', 'from'])
+        
+        # Iterate through points and their IDs
+        for i, j in zip(points, point_ids):
+            # Print point and ID if ID is None
+            if isinstance(j, type(None)):
+                print(i, j)
+            # Skip if the point is outside the bounding box
+            if not shapely.contains(box, i):
                 continue
-    return G
+            if area:
+                # If attribute is spatial (e.g., population density), assign it to edges within hexagons
+                for k in zip(gdf.geometry, gdf['from'], gdf['to']):
+                    if shapely.contains(i, k[0]):  # Check if hexagon contains edge
+                        if feat_name not in G[k[1]][k[2]]:
+                            G[k[1]][k[2]][feat_name] = [j]
+                        else:
+                            G[k[1]][k[2]][feat_name].append(j)
+                    elif shapely.crosses(i, k[0]):  # Check if hexagon crosses edge
+                        if feat_name not in G[k[1]][k[2]]:
+                            G[k[1]][k[2]][feat_name] = [j]
+                        else:
+                            G[k[1]][k[2]][feat_name].append(j)
+            else:
+                # Assign attribute to the nearest edge
+                edge, distance, node_pair = self.find_nearest_edge(gdf.geometry, i, gdf['from'], gdf['to'], box)
+                try:
+                    if feat_name not in G[node_pair[0]][node_pair[1]]:
+                        G[node_pair[0]][node_pair[1]][feat_name] = 1
+                    else:
+                        G[node_pair[0]][node_pair[1]][feat_name] += 1
+                except:
+                    continue
+        return G
 
-def assign_edges(self, G, point_df, box):
-    # Convert the graph into a DataFrame and drop missing 'from' and 'to' columns
-    gdf = graph_to_dataframe(G).dropna(subset=['from', 'to'])
-    
-    # Replace missing values in the points DataFrame with 'None'
-    point_df.fillna('None', inplace=True)
-    
-    # Add a station ID column if it doesn't exist
-    if 'station_id' not in point_df.columns:
-        point_df['station_id'] = point_df.index
-    
-    # Iterate through points and assign them to the nearest edge
-    for i in tqdm(point_df.iterrows(), total=len(point_df)):
-        if not shapely.contains(box, i[1].geometry):
-            continue
-        # Find the nearest edge
-        edge, distance, node_pair = self.find_nearest_edge(gdf.geometry, i[1].geometry, gdf['from'], gdf['to'], box)
-        # Assign station information to the edge
-        try:
-            G[node_pair[0]][node_pair[1]]['station'] = i[1].station_name
-            G[node_pair[0]][node_pair[1]]['station_id'] = i[1].station_id
-        except:
-            print(node_pair, flush=True)
-    return G
+    def assign_edges(self, G, point_df, box):
+        # Convert the graph into a DataFrame and drop missing 'from' and 'to' columns
+        gdf = graph_to_dataframe(G).dropna(subset=['from', 'to'])
+        
+        # Replace missing values in the points DataFrame with 'None'
+        point_df.fillna('None', inplace=True)
+        
+        # Add a station ID column if it doesn't exist
+        if 'station_id' not in point_df.columns:
+            point_df['station_id'] = point_df.index
+        
+        # Iterate through points and assign them to the nearest edge
+        for i in tqdm(point_df.iterrows(), total=len(point_df)):
+            if not shapely.contains(box, i[1].geometry):
+                continue
+            # Find the nearest edge
+            edge, distance, node_pair = self.find_nearest_edge(gdf.geometry, i[1].geometry, gdf['from'], gdf['to'], box)
+            # Assign station information to the edge
+            try:
+                G[node_pair[0]][node_pair[1]]['station'] = i[1].station_name
+                G[node_pair[0]][node_pair[1]]['station_id'] = i[1].station_id
+            except:
+                print(node_pair, flush=True)
+        return G
 
-def make_list_attributes_mean(self, G, attribute):
-    # Calculate the mean for list-type attributes
-    for i in tqdm(my_city.G.edges(data=True)):
-        if i[1] == {}:
-            continue
-        if attribute in i[2]:
-            if isinstance(i[2][attribute], list):  # Check if the attribute is a list
-                i[2][attribute] = np.mean(i[2][attribute])  # Replace list with its mean value
+    def make_list_attributes_mean(self, G, attribute):
+        # Calculate the mean for list-type attributes
+        for i in tqdm(my_city.G.edges(data=True)):
+            if i[1] == {}:
+                continue
+            if attribute in i[2]:
+                if isinstance(i[2][attribute], list):  # Check if the attribute is a list
+                    i[2][attribute] = np.mean(i[2][attribute])  # Replace list with its mean value
 
-def find_nearest_edge(self, linestrings, point, from_node, to_node, box=None):
-    # Initialize variables to find the closest edge
-    shortest_distance = float('inf')
-    closest_edge = None
-    node_pair = None
-    
-    # Check if the point is within the bounding box
-    if shapely.contains(box, point):
-        for linestring, n1, n2 in zip(linestrings, from_node, to_node):
-            # Calculate the distance between the point and the edge
-            distance = linestring.distance(point)
-            if distance < shortest_distance:
-                shortest_distance = distance
-                closest_edge = linestring
-                node_pair = (n1, n2)
-        return closest_edge, shortest_distance, node_pair
+    def find_nearest_edge(self, linestrings, point, from_node, to_node, box=None):
+        # Initialize variables to find the closest edge
+        shortest_distance = float('inf')
+        closest_edge = None
+        node_pair = None
+        
+        # Check if the point is within the bounding box
+        if shapely.contains(box, point):
+            for linestring, n1, n2 in zip(linestrings, from_node, to_node):
+                # Calculate the distance between the point and the edge
+                distance = linestring.distance(point)
+                if distance < shortest_distance:
+                    shortest_distance = distance
+                    closest_edge = linestring
+                    node_pair = (n1, n2)
+            return closest_edge, shortest_distance, node_pair
 
-def construct_line_graph(self):
-    # Convert the graph into its line graph representation
-    H = nx.line_graph(self.G)
-    H.add_nodes_from((node, self.G.edges[node]) for node in H)
-    H.nodes(data=True)
-    self.lg = H  # Store the line graph
+    def construct_line_graph(self):
+        # Convert the graph into its line graph representation
+        H = nx.line_graph(self.G)
+        H.add_nodes_from((node, self.G.edges[node]) for node in H)
+        H.nodes(data=True)
+        self.lg = H  # Store the line graph
 
-def get_listings(self, city, file_path, bounding_box):
-    # Read the listings data
-    data = pd.read_csv(file_path)
-    # Filter listings for the specified city and valid coordinates
-    data = data[data['city'] == city.capitalize()]
-    data = data[data['lat_long'] != "(nan, nan)"]
-    data = data[data['lat_long'] != "(nan,nan)"]
-    
-    # Convert lat_long strings into float values
-    data['lat_long'] = data['lat_long'].apply(lambda x: x.replace('(', '').replace(')', '').split(','))
-    data['geometry'] = data['lat_long'].apply(lambda x: Point(float(x[1]), float(x[0])))
-    
-    # Convert to GeoDataFrame and reproject
-    data_geo = gpd.GeoDataFrame(data, geometry='geometry', crs='EPSG:4326')
-    data_geo = data_geo.to_crs(crs='EPSG:3857')
-    self.get_poly_and_cut(data_geo, bounding_box)
+    def get_listings(self, city, file_path, bounding_box):
+        # Read the listings data
+        data = pd.read_csv(file_path)
+        # Filter listings for the specified city and valid coordinates
+        data = data[data['city'] == city.capitalize()]
+        data = data[data['lat_long'] != "(nan, nan)"]
+        data = data[data['lat_long'] != "(nan,nan)"]
+        
+        # Convert lat_long strings into float values
+        data['lat_long'] = data['lat_long'].apply(lambda x: x.replace('(', '').replace(')', '').split(','))
+        data['geometry'] = data['lat_long'].apply(lambda x: Point(float(x[1]), float(x[0])))
+        
+        # Convert to GeoDataFrame and reproject
+        data_geo = gpd.GeoDataFrame(data, geometry='geometry', crs='EPSG:4326')
+        data_geo = data_geo.to_crs(crs='EPSG:3857')
+        self.get_poly_and_cut(data_geo, bounding_box)
 
-def get_poly_and_cut(self, data, bounding_box):
-    # Filter data points within the bounding box
-    data['keep'] = data['geometry'].apply(lambda x: bounding_box.contains(x))
-    data = data[data['keep']]
-    
-    # Create Voronoi polygons from the data points
-    points = MultiPoint(data['geometry'].to_list())
-    polygons = voronoi_polygons(points)
-    
-    # Assign listing information to Voronoi polygons
-    poly_data = []
-    for i in list(polygons.geoms):
-        temp = data.copy()
-        temp['contains'] = temp['geometry'].apply(lambda x: i.contains(x))
-        temp = temp[temp['contains']]
-        poly_data.append([temp.iloc[0]['price_per_sqm'], i])
-    
-    # Create a GeoDataFrame with polygon geometries
-    poly_geo = gpd.GeoDataFrame(poly_data, columns=['price_per_sqm', 'geometry'], crs='EPSG:3857')
-    poly_geo['geometry'] = poly_geo['geometry'].apply(lambda x: intersection(bounding_box, x))
-    self.geo_data = poly_geo
+    def get_poly_and_cut(self, data, bounding_box):
+        # Filter data points within the bounding box
+        data['keep'] = data['geometry'].apply(lambda x: bounding_box.contains(x))
+        data = data[data['keep']]
+        
+        # Create Voronoi polygons from the data points
+        points = MultiPoint(data['geometry'].to_list())
+        polygons = voronoi_polygons(points)
+        
+        # Assign listing information to Voronoi polygons
+        poly_data = []
+        for i in list(polygons.geoms):
+            temp = data.copy()
+            temp['contains'] = temp['geometry'].apply(lambda x: i.contains(x))
+            temp = temp[temp['contains']]
+            poly_data.append([temp.iloc[0]['price_per_sqm'], i])
+        
+        # Create a GeoDataFrame with polygon geometries
+        poly_geo = gpd.GeoDataFrame(poly_data, columns=['price_per_sqm', 'geometry'], crs='EPSG:3857')
+        poly_geo['geometry'] = poly_geo['geometry'].apply(lambda x: intersection(bounding_box, x))
+        self.geo_data = poly_geo
 
-def get_pop_density(self, city):
-    # Load population density data
-    pop_density = gpd.read_file(f'data/{city}/population_density.gpkg')
-    # Normalize population values and reproject
-    pop_density['population'] = pop_density['population'].apply(lambda x: x / 0.61)
-    pop_density = pop_density.to_crs(epsg=3857)
-    self.pop_density = pop_density  # Store the population density GeoDataFrame
+    def get_pop_density(self, city):
+        # Load population density data
+        pop_density = gpd.read_file(f'data/{city}/population_density.gpkg')
+        # Normalize population values and reproject
+        pop_density['population'] = pop_density['population'].apply(lambda x: x / 0.61)
+        pop_density = pop_density.to_crs(epsg=3857)
+        self.pop_density = pop_density  # Store the population density GeoDataFrame
 
-def read_graph(self):
-    # Load the graph or create a new one
-    self.load_g(self.city)
-    self_loops = list(nx.selfloop_edges(self.G))  # Remove self-loops
-    self.G.remove_edges_from(self_loops)
-    Gcc = sorted(nx.connected_components(self.G), key=len, reverse=True)
-    self.G = self.G.subgraph(Gcc[0])  # Keep the largest connected component
-    print('Self Loops Removed', flush=True)
-    self.lg = nx.line_graph(self.G)
-    self.feats = pd.read_parquet(f'data/{self.city}/linegraph.parquet')
-    node_dict = {tuple(self.feats.iloc[idx, :2]): self.feats.iloc[idx, 2:].to_dict() for idx in range(len(self.feats))}
-    nx.set_node_attributes(self.lg, node_dict)
+    def read_graph(self):
+        # Load the graph or create a new one
+        self.load_g(self.city)
+        self_loops = list(nx.selfloop_edges(self.G))  # Remove self-loops
+        self.G.remove_edges_from(self_loops)
+        Gcc = sorted(nx.connected_components(self.G), key=len, reverse=True)
+        self.G = self.G.subgraph(Gcc[0])  # Keep the largest connected component
+        print('Self Loops Removed', flush=True)
+        self.lg = nx.line_graph(self.G)
+        self.feats = pd.read_parquet(f'data/{self.city}/linegraph.parquet')
+        node_dict = {tuple(self.feats.iloc[idx, :2]): self.feats.iloc[idx, 2:].to_dict() for idx in range(len(self.feats))}
+        nx.set_node_attributes(self.lg, node_dict)
 
-def main(self):
-    # Main pipeline for processing
-    self.load_g(self.city)
-    self.get_pop_density(self.city)
-    self.get_listings(self.city, 'data/listings/clean/listings.csv', self.box)
-    self.assign_attributes(self.bike_stations_df, self.amenity_df)
-    self.make_list_attributes_mean(self.G, 'price_per_sqm')
-    self.make_list_attributes_mean(self.G, 'population')
-    self.construct_line_graph()
-    self.write_graph()
+    def main(self):
+        # Main pipeline for processing
+        self.load_g(self.city)
+        self.get_pop_density(self.city)
+        self.get_listings(self.city, 'data/listings/clean/listings.csv', self.box)
+        self.assign_attributes(self.bike_stations_df, self.amenity_df)
+        self.make_list_attributes_mean(self.G, 'price_per_sqm')
+        self.make_list_attributes_mean(self.G, 'population')
+        self.construct_line_graph()
+        self.write_graph()
 
 
 city = str(sys.argv[1]).lower()
+# city = 'trondheim'
 
 print(city, flush = True)
 
-dir_path = f'bike_network_data/{city}'
-export_path = f'bike_network_data/{city}/processed'
+dir_path = f'data/bike_network_data/{city}'
+export_path = f'data/bike_network_data/{city}/processed'
 
 try:
     combined_df = pd.read_csv(f'{export_path}/preprocessed_bike_rides.csv')
@@ -589,6 +590,5 @@ else:
 city_amenities = gpd.GeoDataFrame(pd.read_json(f'data/{city}/{city}_amenities.json'))
 
 
-if __name__ == '__main__':
-    my_city = CityLineGraph(city, stations_df, city_amenities)
-    my_city.main()
+my_city = CityLineGraph(city, stations_df, city_amenities)
+my_city.main()
